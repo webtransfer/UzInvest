6a732b5294c0ae7e9126545efaf993f3fd4dafe4
Shahzod Rahmon <30922251+MrShahzodbek@users.noreply.github.com>
2018-03-17 11:09:15 +0500

    Update _recovery.php

--- pages/_recovery.php
+++ pages/_recovery.php
@@ -1,19 +1,19 @@
 <?PHP
 ######################################
-# Скрипт Fruit Farm
-# Автор Rufus
+# РЎРєСЂРёРїС‚ Fruit Farm
+# РђРІС‚РѕСЂ Rufus
 # ICQ: 819-374
 # Skype: Rufus272
 ######################################
-$_OPTIMIZATION["title"] = "Восстановление пароля";
-$_OPTIMIZATION["description"] = "Восстановление забытого пароля";
-$_OPTIMIZATION["keywords"] = "Восстановление забытого пароля";
+$_OPTIMIZATION["title"] = "Parolni tiklash";
+$_OPTIMIZATION["description"] = "Unutilgan parolni tiklash";
+$_OPTIMIZATION["keywords"] = "Unutilgan parolni tiklash";
 
 if(isset($_SESSION["user_id"])){ Header("Location: /account"); return; }
 
 ?>
 <div class="s-bk-lf">
-	<div class="acc-title">Восстановление пароля</div>
+	<div class="acc-title">Parolni tiklash</div>
 </div>
 <div class="silver-bk"><div class="clr"></div>	
 <?PHP
@@ -38,27 +38,27 @@ if(isset($_SESSION["user_id"])){ Header("Location: /account"); return; }
 					if($db->NumRows() == 1){
 					$db_q = $db->FetchArray();
 					
-					# Вносим запись в БД
+					# Р’РЅРѕСЃРёРј Р·Р°РїРёСЃСЊ РІ Р‘Р”
 					$db->Query("INSERT INTO db_recovery (email, ip, date_add, date_del) VALUES ('$email',INET_ATON('".$func->UserIP."'),'$time','$tdel')");
 					
-					# Отправляем пароль
+					# РћС‚РїСЂР°РІР»СЏРµРј РїР°СЂРѕР»СЊ
 					$sender = new isender;
 					$sender -> RecoveryPassword($db_q["email"], $db_q["pass"], $db_q["email"]);
 					
-					echo "<center><font color = 'green'><b>Данные для входа отправлены на Email</b></font></center>";
+					echo "<center><font color = 'green'><b>Emailingizga kerakli ma'lumotlar jo'natildi!</b></font></center>";
 					?>
 					</div>
 					<div class="clr"></div>	
 					<?PHP
 					return; 
 					
-					}else echo "<center><font color = 'red'><b>Пользователь с таким Email не зарегистрирован</b></font></center>";
+					}else echo "<center><font color = 'red'><b>Tizimda bunday emailga ega foydalanuvchi mavjud emas</b></font></center>";
 				
-				}else echo "<center><font color = 'red'><b>На Ваш Email или IP уже был отправлен пароль за последние 15 минут</b></font></center>";
+				}else echo "<center><font color = 'red'><b>So'nggi 15 daqiqada tiklash amalga oshirilgan.</b></font></center>";
 				
-			}else echo "<center><font color = 'red'><b>Email указан неверно</b></font></center>";
+			}else echo "<center><font color = 'red'><b>EMail xato kiritilgan</b></font></center>";
 		
-		}else echo "<center><font color = 'red'><b>Символы с картинки введены неверно</b></font></center>";
+		}else echo "<center><font color = 'red'><b>Maxfiy kod xato kiritilgan</b></font></center>";
 	
 	}
 
@@ -68,19 +68,19 @@ if(isset($_SESSION["user_id"])){ Header("Location: /account"); return; }
 <form action="" method="post">
 <table width="550" border="0" cellspacing="0" cellpadding="0">
   <tr>
-    <td align="left" width="250">Email (На него будет выслан пароль):</td>
+    <td align="left" width="250">Email (ro'yhatda o'tishda ko'rsatilgan):</td>
     <td align="left" width="250"><input name="email" type="text" size="25" maxlength="50" value="<?=(isset($_POST["email"])) ? $_POST["email"] : false; ?>"/></td>
   </tr>
   
   <tr>
     <td align="left" width="250" style="padding-top:20px;">
-	<a href="#" onclick="ResetCaptcha(this);"><img src="/captcha.php?rnd=<?=rand(1,10000); ?>"  border="0" style="margin:0;"/></a>
+	<a href="#" onclick="ResetCaptcha(this);"><img src="captcha.php?rnd=<?=rand(1,10000); ?>"  border="0" style="margin:0;"/></a>
 	</td>
-    <td align="left" width="250" style="padding-top:20px;">Введите символы с картинки<input name="captcha" type="text" size="25" maxlength="50" /></td>
+    <td align="left" width="250" style="padding-top:20px;">Rasmdagi maxfiy kodni kiriting <input name="captcha" type="text" size="25" maxlength="50" /></td>
   </tr>
   
   <tr>
-    <td colspan="2" align="center"><BR /><input type="submit" value="Восстановить" style="height: 30px;"></td>
+    <td colspan="2" align="center"><BR /><input type="submit" value="Parolni tiklash" style="height: 30px;"></td>
   </tr>
 </table>
 </form>
