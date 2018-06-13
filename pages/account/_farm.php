536f018f14413d05cedcc7b3e60951a319ed5303
Shahzod Rahmon <30922251+MrShahzodbek@users.noreply.github.com>
2018-06-13 10:43:03 +0500

    Update _farm.php

--- pages/account/_farm.php
+++ pages/account/_farm.php
@@ -1,8 +1,8 @@
-<div class="section_w500">
 <div class="s-bk-lf">
 	<div class="acc-title1">Aksiyalar birjasi</div>
 </div>
-<div class="silver-bk0"><div class="clr"></div>
+
+<div class="silver-bk"><div class="clr"></div>
 <p><font color="black">WEBTRANSFER XK aksiyalari real biznesga yo`naltirilgan sarmoya hisoblanadi.
 Aksiyalarni sotib olish uchun kamida 10'000 so`m sarmoya kiritishingiz kerak.
 Bizda qiyin grafiklar va tushunarsiz shartnomalar yo`q.
@@ -12,54 +12,6 @@ O'zingiz egalik qilayotgan aksiyalardan daromad olishga shoshiling.
 </font></p><br>
 
                                </div>
-                               </div>
-<br>                               
-<div class="section_w500">
-<div class="s-bk-lf">
-	<div class="acc-title1">"UZHOST" aksiyasi haqida</div>
-</div>
-<div class="silver-bk0"><div class="clr"></div>
-<p><font color="black">
-<br>Jami chiqarilgan aksiyalar miqdori: 100 dona
-<br> Birjadagi narxi: 10'000 so`m (1 dona aksiya)
-<br> Daromad: Uzhost.net sof foydasining 25% miqdorida.
-
-</font></p><br>
-
-                               </div>
-                               </div>   
-                               
-                               <br>                               
-<div class="section_w500">
-<div class="s-bk-lf">
-	<div class="acc-title1">"CRYPTO" aksiyasi haqida</div>
-</div>
-<div class="silver-bk0"><div class="clr"></div>
-<p><font color="black">
-<br>Jami chiqarilgan aksiyalar miqdori: 100 dona
-<br> Birjadagi narxi: 25'000 so`m (1 dona aksiya)
-<br> Daromad: Kripto savdolar sof foydasining 20% miqdorida.
-
-</font></p><br>
-
-                               </div>
-                               </div> 
-                               
-                               <br>                               
-<div class="section_w500">
-<div class="s-bk-lf">
-	<div class="acc-title1">"FAST" aksiyasi haqida</div>
-</div>
-<div class="silver-bk0"><div class="clr"></div>
-<p><font color="black">
-<br>Jami chiqarilgan aksiyalar miqdori: 100 dona
-<br> Birjadagi narxi: 50'000 so`m (1 dona aksiya)
-<br> Daromad: UzChange.Ru sof foydasining 20% miqdorida.
-
-</font></p><br>
-
-                               </div>
-                               </div> 
                                
                                
 <br>
@@ -69,24 +21,24 @@ $usid = $_SESSION["user_id"];
 $refid = $_SESSION["referer_id"];
 $usname = $_SESSION["user"];
 
-$db->Query("SELECT * FROM db_users_b WHERE id = '$usid' LIMIT 1");
+$db->Query("SELECT money_b, a_t, b_t, g_t, last_sbor FROM db_users_b WHERE id = '$usid' LIMIT 1");
 $user_data = $db->FetchArray();
 
 $db->Query("SELECT * FROM db_config WHERE id = '1' LIMIT 1");
 $sonfig_site = $db->FetchArray();
 
-# Ïîêóïêà íîâîãî äåðåâà
+# ÐŸÐ¾ÐºÑƒÐ¿ÐºÐ° Ð½Ð¾Ð²Ð¾Ð³Ð¾ Ð´ÐµÑ€ÐµÐ²Ð°
 if(isset($_POST["item"])){
 
 $array_items = array(1 => "a_t", 2 => "b_t", 3 => "c_t", 4 => "d_t", 5 => "e_t", 6 => "f_t", 7 => "g_t");
-$array_name = array(1 => "UZHOST", 2 => "CRYPTO", 3 => "UZCHANGE", 4 => "Ïîìèäîð", 5 => "Êàðòîôåëü", 6 => "Òûêâà", 7 => "FAST");
+$array_name = array(1 => "UZHOST", 2 => "CRYPTO", 3 => "UZCHANGE", 4 => "ÐŸÐ¾Ð¼Ð¸Ð´Ð¾Ñ€", 5 => "ÐšÐ°Ñ€Ñ‚Ð¾Ñ„ÐµÐ»ÑŒ", 6 => "Ð¢Ñ‹ÐºÐ²Ð°", 7 => "FAST");
 $item = intval($_POST["item"]);
 $citem = $array_items[$item];
 $amount = intval($_POST['amount']);
 
 	if(strlen($citem) >= 3){
 		
-		# Ïðîâåðÿåì ñðåäñòâà ïîëüçîâàòåëÿ
+		# ÐŸÑ€Ð¾Ð²ÐµÑ€ÑÐµÐ¼ ÑÑ€ÐµÐ´ÑÑ‚Ð²Ð° Ð¿Ð¾Ð»ÑŒÐ·Ð¾Ð²Ð°Ñ‚ÐµÐ»Ñ
 		if($amount > 0 && $amount <= 1) {
 		$need_money = $sonfig_site["amount_".$citem]*$amount;
 		if($need_money <= $user_data["money_b"]){
@@ -94,20 +46,20 @@ $amount = intval($_POST['amount']);
 			if($user_data["last_sbor"] == 0 OR $user_data["last_sbor"] > ( time() - 60*60*24*1) ){
 				
 				$to_referer = $need_money * 0.1;
-				# Äîáàâëÿåì äåðåâî è ñïèñûâàåì äåíüãè
+				# Ð”Ð¾Ð±Ð°Ð²Ð»ÑÐµÐ¼ Ð´ÐµÑ€ÐµÐ²Ð¾ Ð¸ ÑÐ¿Ð¸ÑÑ‹Ð²Ð°ÐµÐ¼ Ð´ÐµÐ½ÑŒÐ³Ð¸
 				$db->Query("UPDATE db_users_b SET money_b = money_b - $need_money, $citem = $citem + $amount,  
 				last_sbor = IF(last_sbor > 0, last_sbor, '".time()."') WHERE id = '$usid'");
 				$titem = "s_".substr($citem,0,1);
 				$ttime = time()+60*60*24*1;
 				
-				# Âíîñèì çàïèñü î ïîêóïêå
+				# Ð’Ð½Ð¾ÑÐ¸Ð¼ Ð·Ð°Ð¿Ð¸ÑÑŒ Ð¾ Ð¿Ð¾ÐºÑƒÐ¿ÐºÐµ
 				$db->Query("INSERT INTO db_stats_btree (user_id, user, tree_name, amount, date_add, date_del) 
 				VALUES ('$usid','$usname','".$array_name[$item]."','$need_money','".time()."','".(time()+60*60*24*15)."')");
 				
 				$life_time->AddItem($usid,$citem);
 				echo "<center><font color = 'green'><b>Siz $amount dona aksiyani muvaffaqiyatli sotib oldingiz</b></font></center><BR />";
 				
-				$db->Query("SELECT * FROM db_users_b WHERE id = '$usid' LIMIT 1");
+				$db->Query("SELECT money_b, a_t, b_t, g_t FROM db_users_b WHERE id = '$usid' LIMIT 1");
 				$user_data = $db->FetchArray();
 				
 			}else echo "<center><font color = 'red'><b>Aksiya sotib olishdan avval, seyfdan daromadni olish talab etiladi!</b></font></center><BR />";
@@ -128,15 +80,22 @@ $amount = intval($_POST['amount']);
 	</div>
 	
 	<div class="cl-fr-rg" style="padding-left:20px;">
-		<div class="fr-te-gr-title"><font color="red"><b>"UZHOST" aksiyasi</b></font></div>
-		<div class="fr-te-gr"><font color="green">Daromad:</font> <font color="#000000"><?=$sonfig_site["a_in_h"]; ?> tanga / soat</font></div>
+		<div class="fr-te-gr-title"><font color="red"><b>"UZHOST" aksiyasi </font><font color="green"><sup>+1.49%/kun</sup></b></font></div>
+		<div class="fr-te-gr"><font color="green">Daromad:</font> <font color="#000000"><?=$sonfig_site["a_in_h"]*24/10; ?> so'm / kun</font></div>
 		<div class="fr-te-gr"><font color="green">Narxi:</font> <font color="#000000"><?=$sonfig_site["amount_a_t"]; ?><font color="blue"><b> so`m</b></font></div>
 		<div class="fr-te-gr"><font color="green">Muddati:</font> <font color="black">100 kun</font></div>
 		<div class="fr-te-gr"><font color="green">Sizda:</font> <font color="#000000"><?=$user_data["a_t"]; ?> dona</font> aksiya bor</div>
 		<input type="hidden" name="item" value="1" />
+		<?php
+		if($user_data["money_b"] < 10000) {
+		print '<div class="fr-te-gr"><font color="red">Ushbu aksiyani olishingiz uchun<br>hisobingizda yetarli mablag\'<br>mavjud emas!</font></div>';
+		} else {
+		?>
 		
-		<input type="text" name="amount" value="1" style="height: 25px; width: 40px; margin-top:10px;" /> 
+		
+		<input type="hidden" name="amount" value="1" style="height: 25px; width: 40px; margin-top:10px;" /> 
 		<input type="submit" value="Sotib olish" style="height: 25px; margin-top:10px;" />
+		<?php } ?>
 	</div>
 	</form>
 </div>
@@ -148,15 +107,20 @@ $amount = intval($_POST['amount']);
 	</div>
 	
 	<div class="cl-fr-rg" style="padding-left:20px;">
-		<div class="fr-te-gr-title"><font color="red"><b>"CRYPTO" aksiyasi</b></font></div>
-		<div class="fr-te-gr"><font color="green">Daromad:</font> <font color="#000000"><?=$sonfig_site["b_in_h"]; ?> tanga / soat</font></div>
+		<div class="fr-te-gr-title"><font color="red"><b>"CRYPTO" aksiyasi </font><font color="green"><sup>+1.60%/kun</sup></b></font></div>
+		<div class="fr-te-gr"><font color="green">Daromad:</font> <font color="#000000"><?=$sonfig_site["b_in_h"]*24/10; ?> so'm / kun</font></div>
 		<div class="fr-te-gr"><font color="green">Narxi:</font> <font color="#000000"><?=$sonfig_site["amount_b_t"]; ?><font color="blue"><b> so`m</b></font></div>
-		<div class="fr-te-gr"><font color="green">Muddati:</font> <font color="black">50 kun</font></div>
+		<div class="fr-te-gr"><font color="green">Muddati:</font> <font color="black">90 kun</font></div>
 		<div class="fr-te-gr"><font color="green">Sizda:</font> <font color="#000000"><?=$user_data["b_t"]; ?> dona</font> aksiya bor</div>
 		<input type="hidden" name="item" value="2" />
-		
-		<input type="text" name="amount" value="1" style="height: 25px; width: 40px; margin-top:10px;" /> 
+		<?php
+		if($user_data["a_t"] < 5) {
+		print '<div class="fr-te-gr"><font color="red">Ushbu aksiyani olishingiz uchun<br> kamida 5 dona "Uzhost"<br> aksiyasi bo\'lishi kerak!</font></div>';
+		} else {
+		?>
+		<input type="hidden" name="amount" value="1" style="height: 25px; width: 40px; margin-top:10px;" /> 
 		<input type="submit" value="Sotib olish" style="height: 25px; margin-top:10px;" />
+		<?php } ?>
 	</div>
 	</form>
 </div>
@@ -168,18 +132,69 @@ $amount = intval($_POST['amount']);
 	</div>
 	
 	<div class="cl-fr-rg" style="padding-left:20px;">
-		<div class="fr-te-gr-title"><font color="red"><b>"FAST" aksiyasi</b></font></div>
-		<div class="fr-te-gr"><font color="green">Daromad:</font> <font color="#000000"><?=$sonfig_site["g_in_h"]; ?> tanga / soat</font></div>
+		<div class="fr-te-gr-title"><font color="red"><b>"FAST" aksiyasi </font><font color="green"><sup>+2.00%/kun</sup></b></font></div>
+		<div class="fr-te-gr"><font color="green">Daromad:</font> <font color="#000000"><?=$sonfig_site["g_in_h"]*24/10; ?> so'm / kun</font></div>
 		<div class="fr-te-gr"><font color="green">Narxi:</font> <font color="#000000"><?=$sonfig_site["amount_g_t"]; ?><font color="blue"><b> so`m</b></font></div>
-		<div class="fr-te-gr"><font color="green">Muddati:</font> <font color="black">40 kun</font></div>
+		<div class="fr-te-gr"><font color="green">Muddati:</font> <font color="black">60 kun</font></div>
 		<div class="fr-te-gr"><font color="green">Sizda:</font> <font color="#000000"><?=$user_data["g_t"]; ?> dona</font> aksiya bor</div>
 		<input type="hidden" name="item" value="7" />
-		
-		<input type="text" name="amount" value="1" style="height: 25px; width: 40px; margin-top:10px;" /> 
+		<?php
+		if($user_data["b_t"] < 5) {
+		print '<div class="fr-te-gr"><font color="red">Ushbu aksiyani olishingiz uchun<br> kamida 5 dona "CRYPTO"<br>aksiyasi bo\'lishi kerak!</font></div>';
+		} else {
+		?>
+		<input type="hidden" name="amount" value="1" style="height: 25px; width: 40px; margin-top:10px;" /> 
 		<input type="submit" value="Sotib olish" style="height: 25px; margin-top:10px;" />
+		<?php } ?>
 	</div>
 	</form>
 </div>
 
+<br>                               
+<div class="clr"></div>
+<div class="s-bk-lf">
+	<div class="acc-title1">"UZHOST" aksiyasi haqida</div>
+</div>
+<div class="silver-bk0"><div class="clr"></div>
+<p><font color="black">
+<br>Jami chiqarilgan aksiyalar miqdori: 100 dona
+<br> Birjadagi narxi: 10'000 so`m (1 dona aksiya)
+<br> Daromad: Uzhost.net sof foydasining 25% miqdorida.
+
+</font></p><br>
+
+                               </div>
+                                 
+                               
+                               <br>                               
+
+<div class="s-bk-lf">
+	<div class="acc-title1">"CRYPTO" aksiyasi haqida</div>
+</div>
+<div class="silver-bk0"><div class="clr"></div>
+<p><font color="black">
+<br>Jami chiqarilgan aksiyalar miqdori: 100 dona
+<br> Birjadagi narxi: 25'000 so`m (1 dona aksiya)
+<br> Daromad: Kripto savdolar sof foydasining 20% miqdorida.
+
+</font></p><br>
+
+                               </div>
+                               
+                               
+                               <br>                               
+
+<div class="s-bk-lf">
+	<div class="acc-title1">"FAST" aksiyasi haqida</div>
+</div>
+<div class="silver-bk0"><div class="clr"></div>
+<p><font color="black">
+<br>Jami chiqarilgan aksiyalar miqdori: 100 dona
+<br> Birjadagi narxi: 50'000 so`m (1 dona aksiya)
+<br> Daromad: UzChange.Ru sof foydasining 20% miqdorida.
+
+</font></p><br>
+
+                               </div>
 
 <div class="clr"></div>
